904401a6365f19d3a8f3e8042d349202c8dfc646
dnieto23 <dnieto23@gmail.com>
2019-07-19 13:50:19 -0500

    first upload

---
+++ MergeStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la primera palabra");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Digite la segunda palabra");
+            string s2 = Console.ReadLine();
+            Console.WriteLine("las dos palabras son {0} y {1}", s1, s2);
+            StringHandler stringHandler = new StringHandler(s1, s2);
+            Console.WriteLine("El resultado es: ");
+            Console.WriteLine(stringHandler.GetMergedString());
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MergeStrings/StringHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeStrings
+{
+    public class StringHandler
+    {
+        private string FirstText;
+        private string SecondText;
+        private string NumberPart;
+        private string Result;
+
+        public StringHandler(string firstText, string secondText)
+        {
+            FirstText = firstText;
+            SecondText = secondText;
+            Result = string.Empty;
+            NumberPart = string.Empty;
+        }
+
+        private void FillResult(char ch)
+        {
+            if (!Char.IsDigit(ch))
+            {
+                Result += ch.ToString();
+            }
+            else
+            {
+                NumberPart += ch.ToString();
+            }
+        }
+
+        public string GetMergedString()
+        {
+            char[] arr1 = FirstText.ToCharArray();
+            char[] arr2 = SecondText.ToCharArray();
+
+            int i = 0, j = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                FillResult(arr1[i]);
+                FillResult(arr2[j]);
+                i++;
+                j++;
+            }
+            if(i != arr1.Length)
+            {
+                for (int k = i; k < arr1.Length; k++)
+                {
+                    Result += arr1[k].ToString();
+                }
+            }
+            if (i != arr2.Length)
+            {
+                for (int l = j; l < arr2.Length; l++)
+                {
+                    Result += arr2[l].ToString();
+                }
+            }
+
+            return Result + NumberPart;
+        }
+
+
+    }
+}
